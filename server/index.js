7b2588e2372801712543ad3f14b1f7556c79a92e
Ihor Chekhunov <ichekhunov.codica@gmail.com>
2023-03-01 01:59:39 +0100

    [Feature] Created models connect to base

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ require("dotenv").config();
 
 const express = require("express");
 const sequelize = require("./db");
-
+const models = require("./models/models");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 const start = async () => {
   try {
-    await sequelize.authenticate() // connect to bd
-    await sequelize.sync()
+    await sequelize.authenticate(); // connect to bd
+    await sequelize.sync();
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (e) {
     console.log(e);
